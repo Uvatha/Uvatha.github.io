382207165126494756c2323c4421131ad0dd8555
Lemino <adamslawny1@gmail.com>
2021-11-08 23:06:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
-// object that stores the text, source for every card.
-const word_dict = {
-    ear: {text: "This is the Abyss card", source: "https://readwords.s3.us-west-1.amazonaws.com/ear.png"}
-  , eye:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/eye.png"}
-  , knee:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/knee.png"}
-  , leg:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/leg.png"}
-  , shoulder:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/shoulder.png"}
-  , bat:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/bat.png"}
-  , car:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/car.png"}
-  , cat:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/cat.png"}
-  , hand:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/hand.png"}
-  , house:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/house.png"}
-  , nose:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/nose.png"}
-  , toes:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/toes.png"}
-  , star:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/star.png"}
-  , rat:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/rat.png"}
-  , hat:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/hat.png"}
-  , jar:  {text: "Test", source: "https://readwords.s3.us-west-1.amazonaws.com/jar.png"}
-}
-const word_anim_dict = {0:"showup1 2s 1",
-                        1:"showup2 2s 1"}
+// For the thumbnail demo! :]
 
-const img_anim_dict = {0:"showup1 2s 1",
-                       1:"showup2 2s 1"}
+var count = 1
+setTimeout(demo, 500)
+setTimeout(demo, 700)
+setTimeout(demo, 900)
+setTimeout(reset, 2000)
 
-// const word_class_dict = {0:"showup1",
-//                          1:"showup2"}
-
-// const img_class_dict = {0:"showup1;",
-//                         1:"showup2;"}
-
-// /* @keyframes | duration | easing-function | delay |iteration-count | direction | fill-mode 
-// | play-state | nameanimation: 3s ease-in 1s 2 reverse both paused slidein;
-
-let i = 0; // Iterator for word_dict
-let j = 0; // Iterator for anim_dict
+setTimeout(demo, 2500)
+setTimeout(demo, 2750)
+setTimeout(demo, 3050)
 
 
-function showImage() {
-    let img = document.getElementById("cardImg");
-    let lkp = Object.keys(word_dict)[i];
-    img.style.animation = img_anim_dict[j];
-    img.src = word_dict[lkp].source
-
-    if (Object.keys(word_dict).length > i + 1) {i++} else {i = 0};
-    if (Object.keys(word_anim_dict).length > j + 1) {j++} else {j = 0};
+var mousein = false
+function demo() {
+   if(mousein) return
+   document.getElementById('demo' + count++)
+      .classList.toggle('hover')
+   
 }
 
-function showWord() {
-    let wordDiv = document.getElementById("wordDiv")
-    let lkp = Object.keys(word_dict)[i];
-    let img = document.getElementById("cardImg");
+function demo2() {
+   if(mousein) return
+   document.getElementById('demo2')
+      .classList.toggle('hover')
+}
 
-    img.src = ''
+function reset() {
+   count = 1
+   var hovers = document.querySelectorAll('.hover')
+   for(var i = 0; i < hovers.length; i++ ) {
+      hovers[i].classList.remove('hover')
+   }
+}
 
-    wordDiv.innerText = lkp;
-    wordDiv.style.animation = word_anim_dict[j];
-    setTimeout(showImage, 4000);
-} 
+document.addEventListener('mouseover', function() {
+   mousein = true
+   reset()
+})
